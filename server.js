0f9573b27096b7f509eb98cbe1ceba8dbf19a92c
Rob Nova <rnova22@gmail.com>
2024-06-24 13:20:49 -0400

    added basic authentication

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const basicAuth = require('basic-auth');
 const path = require('path');
+
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
+
+const USERNAME = 'admin'; // Change this to your desired username
+const PASSWORD = 'password'; // Change this to your desired password
+
+function auth(req, res, next) {
+    const user = basicAuth(req);
+    if (!user || user.name !== USERNAME || user.pass !== PASSWORD) {
+        res.set('WWW-Authenticate', 'Basic realm="example"');
+        return res.status(401).send('Authentication required.');
+    }
+    next();
+}
+
+app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
-});
-
-app.listen(port, () => {
-    console.log(`Server running at http://127.0.0.1:${port}/`);
+app.listen(PORT, () => {
+    console.log(`Server is running on http://127.0.0.1:${PORT}`);
 });
